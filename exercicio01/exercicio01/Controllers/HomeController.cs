6c15e4e4b41d093dd988e2601426632000919738
Vitor Santos <vitor330y@gmail.com>
2022-02-25 14:21:06 -0300

    Exercicio_01

---
+++ exercicio01/exercicio01/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using exercicio01.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace exercicio01.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ exercicio01/exercicio01/Models/Cliente.cs
@@ -0,0 +1,14 @@
+﻿namespace exercicio01.Models
+{
+    public class Cliente
+    {
+        public String nome { get; set; }
+        public char sexo { get; set; }
+        public String cpf { get; set; }
+        public String rg { get; set; }
+        public String datadenascimento { get; set; }
+        public String endereco { get; set; }
+
+
+    }
+}
